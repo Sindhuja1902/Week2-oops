db1416520bbd18491f1a5aa37c3c25db62bec336
Sindhuja1902 <akulasindhuja41@gmail.com>
2025-08-12 02:12:35 -0700

    Add files via upload

---
+++ Box7.java
@@ -0,0 +1,36 @@
+package co2lab;
+
+class Box7 
+{  
+  double width;
+  double height;
+  double depth;
+
+Box7(double w, double h, double d)  //parameterized
+   {   
+  width = w; height = h; depth = d;
+   }
+
+double volume()
+  { 
+  return width * height * depth;
+   }
+}
+class BoxDemo7
+ {
+ public static void main(String args[]) 
+  { 
+    // declare, allocate, and initialize Box objects
+  Box7 mybox1 = new Box7(10, 20, 15); 
+  Box7 mybox2 = new Box7(3, 6, 9); 
+  double vol; 
+  
+  // get volume of first box 
+  vol = mybox1.volume(); 
+  System.out.println("Volume is " + vol);
+  
+   // get volume of second box 
+  vol = mybox2.volume(); 
+  System.out.println("Volume is " + vol);
+ }
+ }
